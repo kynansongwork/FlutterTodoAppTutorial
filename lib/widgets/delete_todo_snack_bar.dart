303778855bc86db079870f40ab86c74bb2030eb1
Kynan Song <kynan.song@xdesign.com>
2020-06-25 09:44:48 +0100

    Added todo item

---
+++ lib/widgets/delete_todo_snack_bar.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+import 'package:todos_app_core/todos_app_core.dart';
+import 'package:flutter_todos/models/models.dart';
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -2,3 +2,4 @@ export './filter_button.dart';
 export './extra_actions.dart';
 export './tab_selector.dart';
 export './filtered_todos.dart';
+export './todo_item.dart';
